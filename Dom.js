5c2c4434b8d27300a9b9a8b257c04a3d9429c285
Alok277 <sonu.alokknp0512@gmail.comm>
2022-10-27 16:19:58 +0530

    first commit

--- Dom.js
+++ Dom.js
@@ -1,8 +1,10 @@
 var form=document.getElementById('addForm');
 var addList=document.getElementById('items');
+var filter=document.getElementById('filter');
 
 form.addEventListener('submit',addItems);
 addList.addEventListener('click',removeItems);
+filter.addEventListener('keyup',filterItems);
 
 
 
@@ -11,7 +13,7 @@ function addItems(e){
 
     // receive input value;
     var inputValue=document.getElementById('item').value;
-
+   
     // create element
     var li=document.createElement('li');
     
@@ -19,8 +21,23 @@ function addItems(e){
     //create class
     li.className="list-group-item";
 
-    // add text node with input value
-    li.appendChild(document.createTextNode(inputValue));
+   
+
+     // receive input value;
+     var inputValue2=document.getElementById('item2').value;
+   
+     // create element
+     var li=document.createElement('li');
+     
+ 
+     //create class
+     li.className="list-group-item";
+ 
+     // add text node with input value
+     li.appendChild(document.createTextNode(inputValue + inputValue2));
+
+  
+
 
     //create delete button
     var delbtn=document.createElement('button');
@@ -69,10 +86,25 @@ function removeItems(e){
     }
 }
 
-
-
+// filter function
+function filterItems(e){
+    // receive text
+    var text=e.target.value.toLowerCase();
     
+    //get lis
+    var items=addList.getElementsByTagName('li');
+    
+    
+    //convert into array
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent;
+        var itemName2=item.childNodes[1].textContent;
+        
+        if(itemName.toLowerCase().indexOf(text)!=-1 || itemName2.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    });
 
-
-
-
+}
